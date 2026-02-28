1f36eab73c50d798362b9d942662df58785d44f4
nickyhsu <nickyhsu@amazon.com>
2026-02-28 12:15:38 +0800

    fix: resolve unbound variable error and fix comment syntax

--- postgres/diag/shell/pg-major-version-upgrade-precheck.sh
+++ postgres/diag/shell/pg-major-version-upgrade-precheck.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-/*
- *  Copyright 2016 Amazon.com, Inc. or its affiliates. 
- *  All Rights Reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"). 
- *  You may not use this file except in compliance with the License. 
- *  A copy of the License is located at
- * 
- *      http://aws.amazon.com/apache2.0/
- * 
- * or in the "license" file accompanying this file. 
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- * either express or implied. See the License for the specific language governing permissions 
- * and limitations under the License.
-*/
+#/
+ #  Copyright 2016 Amazon.com, Inc. or its affiliates. 
+ #  All Rights Reserved.
+ #
+ #  Licensed under the Apache License, Version 2.0 (the "License"). 
+ #  You may not use this file except in compliance with the License. 
+ #  A copy of the License is located at
+ # 
+ #      http://aws.amazon.com/apache2.0/
+ # 
+ # or in the "license" file accompanying this file. 
+ # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ # either express or implied. See the License for the specific language governing permissions 
+ # and limitations under the License.
+#/
 
 ##################
 #
@@ -46,10 +46,10 @@ set -uo pipefail
 # If the query returns results, the upgrade will fail.
 # ============================================
 
-HOST="${1}"
-PORT="${2}"
-USER="${3}"
-TARGET_VERSION="${4}"
+HOST="${1:-}"
+PORT="${2:-}"
+USER="${3:-}"
+TARGET_VERSION="${4:-}"
 
 # Check required parameters
 if [ -z "${HOST:-}" ] || [ -z "${PORT:-}" ] || [ -z "${USER:-}" ] || [ -z "${TARGET_VERSION:-}" ]; then
